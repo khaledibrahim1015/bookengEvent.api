42bf84f17d677ef64a767ebd5dd82a2520103e83
khaledibrahim1015 <ibrahimkhaled136@gmail.com>
2024-11-12 05:05:44 +0200

    adding authenticatedRouterGroup

--- routes/route.go
+++ routes/route.go
@@ -8,9 +8,16 @@ import (
 func RegisterRoutes(server *gin.Engine) {
 	server.GET("/events", getEvenets)
 	server.GET("/events/:id", getEvent)
-	server.POST("/events", middlewares.Authentication, createEvent)
-	server.PUT("/events/:id", updateEvent)
-	server.DELETE("/events/:id", deleteEvent)
+
+	authenticatedRouterGroup := server.Group("/")
+	authenticatedRouterGroup.Use(middlewares.Authentication)
+	authenticatedRouterGroup.POST("/events", createEvent)
+	authenticatedRouterGroup.PUT("/events/:id", updateEvent)
+	authenticatedRouterGroup.DELETE("/events/:id", deleteEvent)
+
+	// server.POST("/events", middlewares.Authentication, createEvent)
+	// server.PUT("/events/:id", updateEvent)
+	// server.DELETE("/events/:id", deleteEvent)
 	server.POST("/signup", signup)
 	server.POST("/login", login)
 }
